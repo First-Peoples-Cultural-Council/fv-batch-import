0663fe011e8aab439c9d39e1a0dbcc1548f4a6df
Guy McAuliffe <38873380+gmcauliffe@users.noreply.github.com>
2020-01-28 11:09:05 -0800

    Duplicate contributors bug fixed
    SourcesMapper now extends CsvMapper to allow for different cacheDocument.

--- src/main/java/mappers/firstvoices/SourcesMapper.java
+++ src/main/java/mappers/firstvoices/SourcesMapper.java
@@ -3,37 +3,27 @@
  */
 package mappers.firstvoices;
 
+import common.ConsoleLogger;
 import mappers.CsvMapper;
 import mappers.propertyreaders.PropertyReader;
 import org.nuxeo.client.objects.Document;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author dyona
  *
  */
-public class SourcesMapper extends DictionaryCachedMapper {
+public class SourcesMapper extends CsvMapper {
 
     private String linkKey;
+
+    protected String currentCacheId = null;
+    protected String cacheProperty = Properties.TITLE;
     private static Map<String, Map<String,Document>> cache = null;
-
-    @Override
-    protected String getCacheQuery() {
-        // Include all contributors from Dialect
-        return "SELECT * FROM FVContributor WHERE ecm:parentId='" + documents.get("Contributors").getId() + "' AND ecm:isTrashed = 0";
-    }
-
-    private void updateMainDocumentReference(String linkKey, ArrayList<String> sourcesIds) {
-        // Get current sources, and append new values if exists
-        if (documents.get("current").getPropertyValue(linkKey) != null) {
-            ArrayList<String> existingSourcesIds = documents.get("current").getPropertyValue(linkKey);
-            sourcesIds.addAll(existingSourcesIds);
-        }
-        documents.get("current").setPropertyValue(linkKey, sourcesIds);
-    }
 
     public SourcesMapper(int number) {
         super("FVContributor", Columns.CONTRIBUTOR + "_" + number);
@@ -52,6 +42,59 @@ public class SourcesMapper extends DictionaryCachedMapper {
         parentKey = "Contributors";
         this.linkKey = linkKey;
         propertyReaders.add(new PropertyReader(Properties.TITLE, column));
+    }
+
+    @Override
+    protected boolean preCreate() {
+        return true;
+    }
+
+    @Override
+    protected Document getFromCache(Document doc) {
+        currentCacheId = getInstanceCacheKey();
+        if (!cache.containsKey(currentCacheId)) {
+            return null;
+        }
+        String cacheKey = (String) doc.getDirtyProperties().get(cacheProperty);
+        if (cacheKey != null && cache.get(currentCacheId).containsKey(cacheKey)) {
+            return cache.get(currentCacheId).get(cacheKey);
+        }
+        return null;
+    }
+
+    protected String getInstanceCacheKey() {
+        return getClass().getName() + documents.get("Dictionary").getId();
+    }
+    @Override
+    protected void cacheDocument(Document doc) {
+        cache.get(getInstanceCacheKey()).put(doc.getTitle(), doc);
+    }
+
+    @Override
+    public void buildCache() throws IOException {
+        if (cache == null) {
+            cache = new HashMap<String, Map<String,Document>>();
+        }
+        if (cache.containsKey(getInstanceCacheKey())) {
+            return;
+        }
+        cache.put(getInstanceCacheKey(), new HashMap<String, Document>());
+        loadCache(getCacheQuery());
+        ConsoleLogger.out("Caching " + cache.get(getInstanceCacheKey()).size() + " " + getInstanceCacheKey());
+    }
+
+    protected String getCacheQuery() {
+        // Include all contributors from Dialect
+        return "SELECT * FROM FVContributor WHERE ecm:parentId='" + documents.get("Contributors").getId() + "' AND ecm:isTrashed = 0";
+    }
+
+    private void updateMainDocumentReference(String linkKey, ArrayList<String> sourcesIds) {
+        // Get current sources, and append new values if exists
+        if (documents.get("current").getPropertyValue(linkKey) != null) {
+            ArrayList<String> existingSourcesIds = documents.get("current").getPropertyValue(linkKey);
+            sourcesIds.addAll(existingSourcesIds);
+        }
+        documents.get("current").setPropertyValue(linkKey, sourcesIds);
     }
 
     @Override
