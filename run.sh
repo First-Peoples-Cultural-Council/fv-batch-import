08a37c8ea68f3b30e3526b449ee383aec84e9c72
Daniel Yona <daniel@fpcc.ca>
2021-03-09 17:15:17 -0800

    pass vars

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 ## Basic script to process multiple bash files=
 echo "Running against base url: ${BASE_URL} with workspace id ${SITE_WORKSPACE_GUID}"
 
-for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR_KEYWORD}*.csv; do
-    echo "Processing file ${i} (${SKIP_VALIDATION_PARAM}) ...";
-    java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ ${SKIP_VALIDATION_PARAM} -password ${JENKINS_PROD_PASSWORD}
+for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR}*.csv; do
+    echo "Processing file ${i} ($1) using migrator for ${MIGRATOR} ...";
+    java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ $1 -password ${JENKINS_PROD_PASSWORD}
     echo "${i} Completed."
 done
