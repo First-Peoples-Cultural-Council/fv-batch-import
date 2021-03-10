a40c2cfe30d7162f632a16cae289bcacab06f965
Daniel Yona <daniel@fpcc.ca>
2021-03-09 16:14:54 -0800

    Fix unexpected eof

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@
 echo "Running against base url: ${BASE_URL} with workspace id ${SITE_WORKSPACE_GUID}"
 
 for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR_KEYWORD}*.csv; do
-    # Process $i
+
     echo "Processing file ${i}...";
+
     java -Xmx1g -jar target/fv-batch-import-*.jar \
     -url "${BASE_URL}" \
     -username ${JENKINS_PROD_USERNAME} \
@@ -17,5 +18,7 @@ for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR_KEYWORD}*.csv; do
     data/${SITE_WORKSPACE_GUID}/files/ \
     ${SKIP_VALIDATION_PARAM} \
     -password \
-    ${JENKINS_PROD_PASSWORD} \
+    ${JENKINS_PROD_PASSWORD}
+
+    echo "${i} Completed."
 done
