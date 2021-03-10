faf7a2775a486fe11ac04b7b0b65db609ff0fbb3
Daniel Yona <daniel@fpcc.ca>
2021-03-09 17:18:18 -0800

    Ignore errors csv

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ echo "Running against base url: ${BASE_URL} with workspace id ${SITE_WORKSPACE_G
 
 for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR}*.csv; do
     echo "Processing file ${i} ($1) using migrator for ${MIGRATOR} ...";
-    java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ $1 -password ${JENKINS_PROD_PASSWORD}
-    echo "${i} Completed."
+    if [[ "$i" != *"errors"* ]]; then
+      java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ $1 -password ${JENKINS_PROD_PASSWORD}
+      echo "${i} Completed."
+    fi
 done
