e64abc37a3f2364f545890720b4e928e943a5288
Daniel Yona <daniel@fpcc.ca>
2021-04-27 18:19:19 -0700

    Update sh file

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 echo "Running against base url: ${BASE_URL} with workspace id ${SITE_WORKSPACE_GUID}"
 
 for i in ./data/${SITE_WORKSPACE_GUID}/*${MIGRATOR}*.csv; do
-    echo "Processing file ${i} ($1) using migrator for ${MIGRATOR} ...";
+    echo "Processing file ${i} using migrator for ${MIGRATOR} and update strategy $1...";
     if [[ "$i" != *"errors"* ]]; then
-      java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ $1 -password ${JENKINS_PROD_PASSWORD}
+      java -Xmx1g -jar target/fv-batch-import-*.jar -url "${BASE_URL}" -username ${JENKINS_PROD_USERNAME} -domain FV -dialect-id ${SITE_WORKSPACE_GUID} -csv-file $i -data-path data/${SITE_WORKSPACE_GUID}/files/ -updateStrategy $1 $2 -password ${JENKINS_PROD_PASSWORD}
       echo "${i} Completed."
     fi
 done
